d80817cc6ce0befcac2d3d126a332be987f3574d
jingjing <jingjing@DESKTOP-BRRA6S6>
2022-04-09 20:57:16 -0700

    Add project files.

---
+++ razorMoviesMJ/Data/razorMoviesMJContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using razorMoviesMJ.Models;
+
+namespace razorMoviesMJ.Data
+{
+    public class razorMoviesMJContext : DbContext
+    {
+        public razorMoviesMJContext (DbContextOptions<razorMoviesMJContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<razorMoviesMJ.Models.Movie> Movie { get; set; }
+    }
+}
